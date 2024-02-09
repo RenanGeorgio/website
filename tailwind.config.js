b02d6000810eb85043e89682faf968cbb606e4eb
RenanGeorgio <regeo9@hotmail.com>
2024-02-09 19:45:23 -0300

    feat
    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  content: ["./src/components/**/*.{js,ts,jsx,tsx}", "./src/pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -30,6 +30,9 @@ module.exports = {
         md: "0 8px 30px rgba(0, 0, 0, 0.12)",
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
