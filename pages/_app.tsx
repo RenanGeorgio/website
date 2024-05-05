5b424fa8ad31224bdf01f74bc8903bc58ba4e7bc
RenanGeorgio <regeo9@hotmail.com>
2024-05-05 20:00:05 -0300

    up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import { AppProps } from "next/app";
-import "../styles/index.css";
+import { SSRProvider, OverlayProvider } from 'react-aria';
+import '@styles/global.css';
+import '@styles/nprogress.css';
+import '@styles/chrome-bug.css';
+import "../styles/index.css"; // VEM DO ORIGINAL
+import type { AppProps } from 'next/app';
+import NProgress from '@components/nprogress';
+import ResizeHandler from '@components/resize-handler';
+import { useEffect } from 'react';
+import { HMSRoomProvider } from '@100mslive/react-sdk';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.body.classList?.remove('loading');
+  }, []);
+  return (
+    <SSRProvider>
+      <OverlayProvider>
+        <HMSRoomProvider>
+          <Component {...pageProps} />
+          <ResizeHandler />
+          <NProgress />
+        </HMSRoomProvider>
+      </OverlayProvider>
+    </SSRProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,31 @@
-import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
-import Layout from "../components/layout";
-import { getAllPosts } from "../lib/api";
-import Head from "next/head";
-import { CMS_NAME } from "../lib/constants";
-import Post from "../interfaces/post";
+import { useRouter } from 'next/router';
+import { SkipNavContent } from '@reach/skip-nav';
 
-type Props = {
-  allPosts: Post[];
-};
+import Page from '@components/page';
+import ConfContent from '@components/index';
+import { META_DESCRIPTION } from '@lib/constants';
 
-export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+export default function Conf() {
+  const { query } = useRouter();
+  const meta = {
+    title: 'Demo - Virtual Event Starter Kit',
+    description: META_DESCRIPTION
+  };
+  const ticketNumber = query.ticketNumber?.toString();
+  const defaultUserData = {
+    id: query.id?.toString(),
+    ticketNumber: ticketNumber ? parseInt(ticketNumber, 10) : undefined,
+    name: query.name?.toString(),
+    username: query.username?.toString()
+  };
+
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
+    <Page meta={meta} fullViewport>
+      <SkipNavContent />
+      <ConfContent
+        defaultUserData={defaultUserData}
+        defaultPageState={query.ticketNumber ? 'ticket' : 'registration'}
+      />
+    </Page>
   );
 }
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
-
-  return {
-    props: { allPosts },
-  };
-};
