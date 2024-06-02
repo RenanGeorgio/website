e2666a0a0654f16426ec1c4ffafb7498c0653083
RenanGeorgio <regeo9@hotmail.com>
2024-06-02 16:42:45 -0300

    test

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-import { Heading, VStack, HStack } from '@chakra-ui/react';
+import { Heading, VStack, HStack, Box, Container as Chakrainer, SimpleGrid, Stack } from '@chakra-ui/react';
 import { COPYRIGHT_HOLDER, SITE_NAME, CODE_OF_CONDUCT, LEGAL_URL, TERMS_URL } from '@assets/constants';
 import Newsletter from '@components/newsletter';
 import { Menu } from '@components/menu';
@@ -70,78 +70,136 @@ const Footer: React.FC<any> = (data: typeof footerObj): JSX.Element => {
           ))}
           
           
-          
-          <div className={styles['footer-legal']}>
-            <div className={styles['footer-center-group']}>
-              {LEGAL_URL && (
-                <>
+          <Box w="full">
+            <Chakrainer as={Stack} maxW={'1000px'} py={10}>
+              <SimpleGrid columns={[1, 2, 4]} spacing={8} px={2}>
+              <Stack spacing={6}>
+                <HStack>
+                  <IconCompany />
+                  <Heading as="p" fontSize="lg">
+                   Ignai-bot.io
+                  </Heading>
+                </HStack>
+            </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Product</ListHeader>
+            <TextLink href={'/pricing'} isExternal>
+              Status
+            </TextLink>
+            <TextLink href={'/pricing'} isExternal>
+              Documentation
+            </TextLink>
+            <TextLink href={'/pricing'} isExternal>
+              Roadmap
+            </TextLink>
+            <TextLink href={'/pricing'}>Pricing</TextLink>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Community</ListHeader>
+            <TextLink href={'/pricing'} isExternal>
+              Discord
+            </TextLink>
+            <TextLink href={'/pricing'} isExternal>
+              GitHub repository
+            </TextLink>
+            <TextLink href={'/pricing'} isExternal>
+              Twitter
+            </TextLink>
+            <TextLink href={'/pricing'} isExternal>
+              LinkedIn
+            </TextLink>
+            <TextLink href="/oss-friends">OSS Friends</TextLink>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Company</ListHeader>
+            <TextLink href="/about">About</TextLink>
+            <TextLink href="mailto:support@typebot.io">Contact</TextLink>
+            <TextLink href={'/terms-of-service'}>Terms of Service</TextLink>
+            <TextLink href={'/privacy-policies'}>Privacy Policy</TextLink>
+          </Stack>
+
+
+
+
+
+
+
+
+
+
+                <div className={styles['footer-legal']}>
+                  <div className={styles['footer-center-group']}>
+                    {LEGAL_URL && (
+                      <>
+                        <p className={styles['footer-paragraph']}>
+                          <a
+                            href={LEGAL_URL}
+                            className={styles['footer-link']}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Politica de Privacidade
+                          </a>
+                        </p>
+                      </>
+                    )}
+                    {TERMS_URL && (
+                      <>
+                        <p className={styles['footer-paragraph']}>
+                          <a
+                            href={TERMS_URL}
+                            className={styles['footer-link']}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Termos de Serviço
+                          </a>
+                        </p>
+                      </>
+                    )}
+                    <p className={styles['footer-paragraph']}>
+                      <a
+                        href={CODE_OF_CONDUCT}
+                        className={styles['footer-link']}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Codigo de Conduta
+                      </a>
+                    </p>
+                  </div>
+                </div>
+                
+                
+                <div className={styles['footer-center-group']}>
                   <p className={styles['footer-paragraph']}>
-                    <a
-                      href={LEGAL_URL}
-                      className={styles['footer-link']}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Politica de Privacidade
-                    </a>
+                    <TextLink href="/about">Sobre</TextLink>
                   </p>
-                </>
-              )}
-              {TERMS_URL && (
-                <>
+                  <div className={styles['footer-separator']} />
                   <p className={styles['footer-paragraph']}>
-                    <a
-                      href={TERMS_URL}
-                      className={styles['footer-link']}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Termos de Serviço
-                    </a>
+                    <TextLink href="mailto:contato@diamondbigger.com">Contatar</TextLink>
                   </p>
-                </>
-              )}
-              <p className={styles['footer-paragraph']}>
-                <a
-                  href={CODE_OF_CONDUCT}
-                  className={styles['footer-link']}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Codigo de Conduta
-                </a>
-              </p>
-            </div>
-          </div>
-          
-          
-            <div className={styles['footer-center-group']}>
-              <p className={styles['footer-paragraph']}>
-                <TextLink href="/about">Sobre</TextLink>
-              </p>
-              <div className={styles['footer-separator']} />
-              <p className={styles['footer-paragraph']}>
-                <TextLink href="mailto:contato@diamondbigger.com">Contatar</TextLink>
-              </p>
-            </div>
-          
-          
-          <div className={styles['footer-hostedby']}>
-            <HostedByDiamondbigger />
-          </div>
-          <Heading as="p" fontSize="lg">
-            (27) 3325-7208
-          </Heading>
-          <Heading as="p" fontSize="lg">
-            contato@ignai.com.br
-          </Heading>
-          <Heading as="p" fontSize="lg">
-            Rua Olympio R, 116 - Jabour 
-          </Heading>
-          <Heading as="p" fontSize="lg">
-            Vitória - ES
-          </Heading>
-          
+                </div>
+                
+                
+                <div className={styles['footer-hostedby']}>
+                  <HostedByDiamondbigger />
+                </div>
+                <Heading as="p" fontSize="lg">
+                  (27) 3325-7208
+                </Heading>
+                <Heading as="p" fontSize="lg">
+                  contato@ignai.com.br
+                </Heading>
+                <Heading as="p" fontSize="lg">
+                  Rua Olympio R, 116 - Jabour 
+                </Heading>
+                <Heading as="p" fontSize="lg">
+                  Vitória - ES
+                </Heading>
+              </SimpleGrid>
+            </Chakrainer>
+          </Box>
          
           <div className="footer--extras">
             <div className="footer--disclaimer">
